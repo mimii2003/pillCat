13204bc4a6d99d69a19ce9071e96f49b86f3fdbb
mimii2003 <mbarreira@sga.pucminas.br>
2023-11-11 12:24:05 -0300

    inicializa integracai com o back

---
+++ PillInfo.swift
@@ -0,0 +1,18 @@
+//
+//  PillInfo.swift
+//  techVerse
+//
+//  Created by Milena Soares on 04/11/23.
+//
+
+import SwiftUI
+
+struct PillInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PillInfo()
+}
